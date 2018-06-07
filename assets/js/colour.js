76aab73b4743f8f3b4c07d5f72ca2ccdb5dabac4
Konstantinos Stouras <konstantinos.stouras@insead.edu>
2018-06-07 19:10:30 -0400

    Update colour.js

--- assets/js/colour.js
+++ assets/js/colour.js
@@ -1,4 +1,4 @@
-function MouseOver(){
+function MouseOver(event){
                 setInterval(function(){ 
                     var red = Math.round(Math.random() * 255);
                     var green = Math.round(Math.random() *255);
@@ -6,14 +6,14 @@ function MouseOver(){
 
                     var bg = "padding: 0; background: rgb("+red+","+green+","+blue+");";
                     var element = document.getElementById("header-image");
-                    element.style = bg;}, 100); 
+                    element.style = bg;}, 1000); 
 }
 
 
 
 
-function MouseOut(){
-  document.getElementById("header-image").style = "padding: 0; background: black;"
+function MouseOut(event){
+  document.getElementById("header-image").style = "padding: 0; background: black;";
 }
        
      
