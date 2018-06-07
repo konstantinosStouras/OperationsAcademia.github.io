8f0218a207399a5dbb90944a6b8991b3004075a6
Konstantinos Stouras <konstantinos.stouras@insead.edu>
2018-06-07 22:46:04 +0300

    Add files via upload

---
+++ assets/js/colour.js
@@ -0,0 +1,25 @@
+
+
+function MouseOver(event){
+     setInterval(function(){ 
+        var red = Math.round(Math.random() * 255);
+        var green = Math.round(Math.random() *255);
+        var blue = Math.round(Math.random() *255);
+        var bg = "padding: 0; background: rgb("+red+","+green+","+blue+");";
+        var element = document.getElementById("header-image");
+        element.style = bg;}, 1000); 
+ }
+
+
+function MouseOut(event) {
+    setInterval(function() {
+        document.getElementById("header-image").style = "padding: 0; background: black;"
+    },0);
+}
+
+// function that renews the above functions
+
+
+
+
+ 
