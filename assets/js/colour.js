608e14b6e442a7b722bbb03ffffe154b71bbc792
Konstantinos Stouras <konstantinos.stouras@insead.edu>
2018-06-08 01:46:42 +0300

    Add files via upload

--- assets/js/colour.js
+++ assets/js/colour.js
@@ -1,32 +1,20 @@
+                function MouseOver(){
+                setInterval(function(){ 
+                    var red = Math.round(Math.random() * 255);
+                    var green = Math.round(Math.random() *255);
+                    var blue = Math.round(Math.random() *255);
 
-
-function MouseOver(event){
-     setInterval(function(){ 
-        var red = Math.round(Math.random() * 255);
-        var green = Math.round(Math.random() *255);
-        var blue = Math.round(Math.random() *255);
-        var bg = "padding: 0; background: rgb("+red+","+green+","+blue+");";
-        var element = document.getElementById("header-image");
-        element.style = bg;}, 0); 
- }
-
-function MouseOverNew(event){
-        var red = Math.round(Math.random() * 255);
-        var green = Math.round(Math.random() *255);
-        var blue = Math.round(Math.random() *255);
-        event.target.setAttributeNS(null, "fill", "rgb("+ red +","+ green +","+ blue +")");
- }
-
-
-function MouseOut(event) {
-    setInterval(function() {
-        document.getElementById("header-image").style = "padding: 0; background: black;"
-    },0);
-}
-
-// function that renews the above functions
+                    var bg = "padding: 0; background: rgb("+red+","+green+","+blue+");";
+                    var element = document.getElementById("foot-img");
+                    element.style = bg;},1000); 
+                }
 
 
 
 
- 
+            function MouseOut(){
+                document.getElementById("foot-img").style = "padding: 0; background: black;"
+            }
+       
+     
+     
