aa1a75bf60f55052bda8f72968567eba1cc8fc46
Konstantinos Stouras <konstantinos.stouras@insead.edu>
2024-09-24 20:55:44 +0300

    ok

---
+++ back up/2024/assets/js/navigationMenu.js
@@ -0,0 +1,39 @@
+/*
+    Use this tool to clean the CSS code: https://www.10bestdesign.com/dirtymarkup/css/
+*/
+
+/* document.getElementById( "nav" ).innerHTML = '<ul>' + '<li>' +
+	'<a href="survey">Annual job market survey</a><ul>' +
+	'<li><a href="survey-faqs">Survey FAQs</a></li>' + '</ul>' + '</li>' + '<li>' +
+	'<a href="jobs">2024-2025 job market</a>' + '<ul>' +
+	'<li><a href="candidates">Candidates</a></li>' +
+	'<li><a href="jobs">Job postings</a></li>' +
+	'<li><a href="placements">Confirmed placements</a></li>' + '</ul>' +
+	'</li>' + '<li>' + '<a href="resources-for-candidates">Resources</a>' + '<ul>'+
+	'<li><a href="resources-for-candidates">For job candidates</a></li>' +
+	'<li><a href="universities">Universities</a></li>'  +
+	'<li><a href="previous-markets">Previous job markets</a></li>' +
+	'<li><a href="recent-faculty">Recent junior faculty</a></li>' + '</ul>' + '</li>' + '<li>' +
+	'<a href="faqs">About</a>' + '<ul>' + '<li><a href="directors-and-contributors">Directors and contributors</a></li>' + '<li><a href="faqs">FAQs</a></li>' +
+	'<li><a href="https://operationsacademia.wordpress.com">Blog</a></li>' +
+	'<li><a href="http://a.co/ev61V45">Donate (in books)</a></li>' + 
+	'<li><a href="analytics">Analytics</a></li>' + '<li><a href="https://operationsacademia.uservoice.com/">Feedback</a></li>' + '<li><a href="contact">Contact</a></li>' + '</ul>' +
+	'</li>' + '</ul>'; */
+	
+	document.getElementById( "nav" ).innerHTML = '<ul>' + '<li>' +
+	'<a href="survey">Annual job market survey</a><ul>' +
+	'<li><a href="survey-faqs">Survey FAQs</a></li>' + '</ul>' + '</li>' + '<li>' +
+	'<a href="jobs">2024-2025 job market</a>' + '<ul>' +
+	'<li><a href="candidates">Candidates</a></li>' +
+	'<li><a href="jobs">Job postings</a></li>' +
+	'<li><a href="placements">Confirmed placements</a></li>' + '</ul>' +
+	'</li>' + '<li>' + '<a href="resources-for-candidates">Resources</a>' + '<ul>'+
+	'<li><a href="resources-for-candidates">For job candidates</a></li>' +
+	'<li><a href="universities">Universities</a></li>'  +
+	'<li><a href="previous-markets">Previous job markets</a></li>' +
+	'<li><a href="recent-faculty">Recent junior faculty</a></li>' + '</ul>' + '</li>' + '<li>' +
+	'<a href="faqs">About</a>' + '<ul>' + '<li><a href="directors-and-contributors">Directors and contributors</a></li>' + '<li><a href="faqs">FAQs</a></li>' +
+	'<li><a href="http://a.co/ev61V45">Donate (in books)</a></li>' + 
+	'<li><a href="analytics">Analytics</a></li>' + '<li><a href="contact">Contact</a></li>' + '</ul>' +
+	'</li>' + '</ul>';
+	
